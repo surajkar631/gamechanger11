4860c3734b456e9739c8f09b18d6d118e7148ecb
surajkar631 <surajmumarol1234@gmail.com>
2025-03-20 13:46:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent default submission
+    window.location.href = "nextpage.html"; // Redirect manually
+});
